76d46d6978b376901ccf0ad11c543290e4764e40
Timophei <tima@testking.ru>
2017-12-14 16:35:22 +0300

    real photos in order items with glide

--- app/src/main/java/com/another1dd/galleryapp/ui/adapters/order/OrderAdapter.kt
+++ app/src/main/java/com/another1dd/galleryapp/ui/adapters/order/OrderAdapter.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import com.another1dd.galleryapp.R
 import com.another1dd.galleryapp.models.Image
 import com.another1dd.galleryapp.utils.rx.RxArrayList
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.order_item.view.*
 
 class OrderAdapter(private val context: Context,
                    private val selectedImages: RxArrayList<Image>) : RecyclerView.Adapter<OrderAdapter.ViewHolder>() {
@@ -21,10 +23,13 @@ class OrderAdapter(private val context: Context,
     }
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-
+        holder?.bindImage(selectedImages[position])
     }
 
     inner class ViewHolder(view: View,
                            private val orderAdapter: OrderAdapter) : RecyclerView.ViewHolder(view) {
+        fun bindImage(image: Image) {
+            Glide.with(context).load(image.path).centerCrop().into(itemView.orderItemPhotoIv)
+        }
     }
 }
